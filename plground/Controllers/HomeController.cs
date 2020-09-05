ae616d1dd58b3d8fc7f17d2677bac38fc1150f15
aenen <yaroslav.golyuk@gmail.com>
2020-09-05 21:29:11 +0300

    Authorization and page with videos

--- plground/Controllers/HomeController.cs
+++ plground/Controllers/HomeController.cs
@@ -1,16 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace plground.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        private readonly string _pass = ConfigurationManager.AppSettings["pass"];
+
+        [Authorize]
         public ActionResult Index()
         {
+            var filenames = Directory.EnumerateFiles(Server.MapPath("~/Videos"))
+                .Select(x => Path.GetFileName(x))
+                .OrderBy(x => x)
+                .ToList();
+
+            return View(filenames);
+        }
+
+        public ActionResult Authorize()
+        {
+            FormsAuthentication.SignOut();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(string password)
+        {
+            if (_pass == password)
+                FormsAuthentication.RedirectFromLoginPage(_pass, true);
+
             return View();
         }
     }
